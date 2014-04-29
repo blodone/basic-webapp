5e3473bcee396c04924b71e783bb0f2e9b5795a5
Stefan Feser <stefan@feelleicht.de>
2014-04-29 12:45:58 +0200

    Added imagemin, htmlmin and processhtml as grunt tasks to generate dist directory with clean html files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,49 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+        imagemin: {
+            dynamic: { 
+                files: [{
+                    expand: true,                 
+                    cwd: 'assets/images/',                  
+                    src: ['**/*.{png,jpg,gif}'],  
+                    dest: 'dist/assets/images' 
+                }]
+            }
+        },
+
+        processhtml: {
+            build: {
+                files: {
+                    'dist/index.html': ['index.html']
+                }
+            }
+        },
+
+        htmlmin: {                                     
+            index: {                                      
+                options: {                                
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                
+                    'dist/index.html': 'dist/index.html',
+                    'dist/404.html': '404.html',
+                    'dist/500.html': '500.html',
+                },
+            },
+            views: {
+                files: [
+                    {
+                        expand: true,     // Enable dynamic expansion.
+                        cwd: 'views/',      // Src matches are relative to this path.
+                        src: ['**/*.html'], // Actual pattern(s) to match.
+                        dest: 'dist/views/',   // Destination path prefix.
+                    },
+                ],
+            }
+        },
         
         watch: {
             options: {
@@ -66,15 +109,17 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-notify');
 
     // Default task(s).
     grunt.registerTask('default', [
-        'concat', 
-        'uglify', 
+        'concat',
         'jshint', 
-        'less', 
+        'less:development', 
         'watch'
     ]);
 
@@ -82,7 +127,10 @@ module.exports = function(grunt) {
         'concat', 
         'uglify', 
         'jshint', 
-        'less'
+        'less',
+        'imagemin',
+        'processhtml',
+        'htmlmin'
     ]);
 
 };
