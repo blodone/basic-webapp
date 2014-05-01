b60011cb83f827f74721636eb1fa52fafbfd0ff5
Stefan Feser <Stefan@Stefans-MacBook-Air-4.local>
2014-05-01 12:47:49 +0200

    Moved js files into app/ directory

---
+++ assets/app/app.js
@@ -0,0 +1,20 @@
+(function(window, angular, undefined){ 'use strict';
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, ['APPNAME']);
+    });
+
+    /*
+     *  Load all modules needed
+     */
+    var app = angular.module('APPNAME', [
+        'ngRoute'
+    ]);
+
+    /*
+     *  Angular routing
+     */
+    app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {templateUrl: 'views/equipment-list.html', controller: 'AnyController'});
+        $routeProvider.when('/start', {templateUrl: 'views/equipment-list.html', controller: 'AnyController'});
+    }]);
+})(window, window.angular);
